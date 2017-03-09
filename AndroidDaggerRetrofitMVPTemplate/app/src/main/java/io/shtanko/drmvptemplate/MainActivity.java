4191371e76270532d78272badc63d8b57d4fb6a2
Alexey <adamluissean@gmail.com>
2017-03-09 20:57:22 +0200

    Initial AndroidDaggerRetrofitMVPTemplate

---
+++ AndroidDaggerRetrofitMVPTemplate/app/src/main/java/io/shtanko/drmvptemplate/MainActivity.java
@@ -0,0 +1,12 @@
+package io.shtanko.drmvptemplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
