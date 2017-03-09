277e34563f507e1b12676e170ee5ca0e36dd1fc9
Alexey <adamluissean@gmail.com>
2017-03-09 08:50:18 +0200

    [WIP] Add BaseFragment

--- AndroidBaseTemplate/app/src/main/java/io/shtanko/basetemplate/ui/activity/MainActivity.java
+++ AndroidBaseTemplate/app/src/main/java/io/shtanko/basetemplate/ui/activity/MainActivity.java
@@ -7,9 +7,9 @@ import io.shtanko.basetemplate.R;
 
 public class MainActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
 }
