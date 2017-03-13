b010d0e46c5cb130f8fba3d400de818b1ec53022
Alexey <adamluissean@gmail.com>
2017-03-14 00:06:31 +0200

    Refactor clean.sh

--- clean.sh
+++ clean.sh
@@ -3,26 +3,24 @@
 build_dir_count=0
 
 function clean(){
-	echo "$1"
-for f in $1/*; do
-if [ -d $f ]; then
-cd $f
-if [ -d $f/build ]; then
-let "build_dir_count = $build_dir_count + 1"
-echo "build dir is found, handle remove it if needed"
-fi
-clean $f
-fi
-done
+	for f in $1/*; do
+		if [ -d $f ]; then
+			cd $f
+			if [ -d $f/build ]; then
+				let "build_dir_count = $build_dir_count + 1"
+				echo "build dir is found this: $f"
+			fi
+		clean $f
+		fi
+	done
 }
 
 function scan(){
-	echo "$1"
-for f in $1/*; do
-if [ -d $f ]; then
-clean $f
-fi
-done
+	for f in $1/*; do
+		if [ -d $f ]; then
+			clean $f
+		fi
+	done
 }
 
 scan $(pwd)
