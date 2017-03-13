5ed4663fd218fec603c2f77c236d7b3792e06b99
Alexey <adamluissean@gmail.com>
2017-03-14 00:03:44 +0200

    Add clean function to clean.sh

--- clean.sh
+++ clean.sh
@@ -1,12 +1,30 @@
 #! /usr/bin/env bash
 
+build_dir_count=0
+
+function clean(){
+	echo "$1"
+for f in $1/*; do
+if [ -d $f ]; then
+cd $f
+if [ -d $f/build ]; then
+let "build_dir_count = $build_dir_count + 1"
+echo "build dir is found, handle remove it if needed"
+fi
+clean $f
+fi
+done
+}
+
 function scan(){
 	echo "$1"
 for f in $1/*; do
 if [ -d $f ]; then
-echo "$f"
+clean $f
 fi
 done
 }
 
 scan $(pwd)
+echo ""
+echo "Result: build dir count: $build_dir_count"
