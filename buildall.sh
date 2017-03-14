f51f53c355441516fea7ad85e5c3ce7e2e66844f
Alexey Shtanko <adamluissean@gmail.com>
2017-03-14 12:47:40 +0200

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+echo "Script is runing"
+echo "project path: $(pwd)"
+echo "OS: $OSTYPE"
+echo "$(ls -a)"
+echo "$HOME"
+echo ""
+
 function build() {
 	echo "build project $1"
 	cd $1 && chmod +x ./gradlew && ./gradlew build
