51826205581e881d591fe8fc20477abfd9460992
Alexey <adamluissean@gmail.com>
2017-03-14 12:58:43 +0200

    Add print function to buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -7,6 +7,15 @@ echo "$(ls -a)"
 echo "$HOME"
 echo ""
 
+function print() {
+	for f in $1/*; do
+		echo "$f"
+		if [ -d $f ]; then
+			print $f
+		fi
+	done
+}
+
 function build() {
 	echo "build project $1"
 	cd $1 && chmod +x ./gradlew && ./gradlew build
@@ -14,8 +23,10 @@ function build() {
 
 function scan() {
 	for f in $1/*; do
+		echo "scan $f"
 		if [ -d $f ]; then
 			if [ -f $f/gradlew ]; then
+				print $f
 				build $f
 			fi
 		fi
