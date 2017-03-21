c23437b7ccc718eb30372b6f4e49cc8b1dd4e642
Alexey <adamluissean@gmail.com>
2017-03-21 23:55:37 +0200

    [WIP] Initial AndroidStorageTemplate

---
+++ AndroidStorageTemplate/app/src/main/java/io/shtanko/storagetemplate/App.java
@@ -0,0 +1,6 @@
+package io.shtanko.storagetemplate;
+
+import android.app.Application;
+
+public class App extends Application {
+}
---
+++ AndroidStorageTemplate/app/src/main/java/io/shtanko/storagetemplate/MainActivity.java
@@ -0,0 +1,6 @@
+package io.shtanko.storagetemplate;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
