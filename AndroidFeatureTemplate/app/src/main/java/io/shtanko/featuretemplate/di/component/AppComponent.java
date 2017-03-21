c66d3165819d81648909c6e4ef4339f993aba5e6
Alexey <adamluissean@gmail.com>
2017-03-21 23:34:38 +0200

    Add NavigationModule

--- AndroidFeatureTemplate/app/src/main/java/io/shtanko/featuretemplate/di/component/AppComponent.java
+++ AndroidFeatureTemplate/app/src/main/java/io/shtanko/featuretemplate/di/component/AppComponent.java
@@ -1,12 +1,13 @@
 package io.shtanko.featuretemplate.di.component;
 
 import dagger.Component;
+import io.shtanko.featuretemplate.di.module.NavigationModule;
 import io.shtanko.featuretemplate.ui.activity.MainActivity;
 import io.shtanko.featuretemplate.di.module.AppModule;
 import io.shtanko.featuretemplate.di.module.NetModule;
 import javax.inject.Singleton;
 
-@Singleton @Component(modules = { AppModule.class, NetModule.class })
+@Singleton @Component(modules = { AppModule.class, NetModule.class, NavigationModule.class })
 public interface AppComponent {
   void inject(MainActivity activity);
 }
--- AndroidFeatureTemplate/app/src/main/java/io/shtanko/featuretemplate/di/module/NavigationModule.java
+++ AndroidFeatureTemplate/app/src/main/java/io/shtanko/featuretemplate/di/module/NavigationModule.java
@@ -1,4 +1,26 @@
 package io.shtanko.featuretemplate.di.module;
 
-public class NavigationModule {
+import com.arellomobile.mvp.presenter.ProvidePresenter;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+import ru.terrakok.cicerone.Cicerone;
+import ru.terrakok.cicerone.NavigatorHolder;
+import ru.terrakok.cicerone.Router;
+
+@Module public class NavigationModule {
+
+  private Cicerone<Router> instance;
+
+  public NavigationModule() {
+    instance = Cicerone.create();
+  }
+
+  @Provides @Singleton Router provideRouter() {
+    return instance.getRouter();
+  }
+
+  @Provides @Singleton NavigatorHolder provideNavigationHolder() {
+    return instance.getNavigatorHolder();
+  }
 }
