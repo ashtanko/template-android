1123acf3fc5992ce6b7e811c1ca429dcccc73893
Alexey <adamluissean@gmail.com>
2017-04-04 10:15:10 +0300

    [WIP] [DEPENDENCIES] Update AndroidTestTemplate + dependencies.gradle

---
+++ AndroidTestTemplate/app/src/main/java/io/shtanko/androidtesttemplate/MainActivity.java
@@ -0,0 +1,7 @@
+package io.shtanko.androidtesttemplate;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+
+@SuppressLint("Registered") public class MainActivity extends AppCompatActivity {
+}
