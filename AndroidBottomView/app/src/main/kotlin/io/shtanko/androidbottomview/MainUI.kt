c3d4eb4893206e079c6c4aa705be6d132785ead6
AdamLuisSean <adamluissean@gmail.com>
2017-05-08 22:59:59 +0300

    [WIP] Add bottom navigation

--- AndroidBottomView/app/src/main/kotlin/io/shtanko/androidbottomview/MainUI.kt
+++ AndroidBottomView/app/src/main/kotlin/io/shtanko/androidbottomview/MainUI.kt
@@ -1,22 +1,36 @@
 package io.shtanko.androidbottomview
 
+import android.view.Gravity
 import android.view.View
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.design.bottomNavigationView
+import org.jetbrains.anko.design.coordinatorLayout
 import org.jetbrains.anko.frameLayout
-import org.jetbrains.anko.linearLayout
 import org.jetbrains.anko.matchParent
 
 class MainUI<T> : AnkoComponent<T> {
 
+  val CONTENT_FRAME = R.id.main_container
+
   override fun createView(ui: AnkoContext<T>): View = with(ui) {
-    linearLayout {
+    coordinatorLayout {
 
       frameLayout {
-        backgroundResource = R.color.material_blue_grey_800
+        id = CONTENT_FRAME
+        lparams(matchParent, matchParent)
+      }
 
-      }.lparams(width = matchParent, height = 200)
+      bottomNavigationView() {
+
+        inflateMenu(R.menu.navigation_menu)
+        fitsSystemWindows = true
+
+
+      }.lparams(matchParent) {
+        anchorGravity = Gravity.BOTTOM
+        anchorId = CONTENT_FRAME
+      }
 
     }
   }
